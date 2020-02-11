ccf36349ec7b84245e744480f711b014a4d8d287
Ridhwan Adagiri <ibrahimridwan47@gmail.com>
2020-02-11 08:43:35 -1100

    Updates

---
+++ precache-manifest.b086330b3e41102b92e8144fa796e62a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d274b4b05ce8c0d2fe17f742484a084",
+    "url": "/simpu/index.html"
+  },
+  {
+    "revision": "dd210e35276f969f9037",
+    "url": "/simpu/static/css/main.c231336a.chunk.css"
+  },
+  {
+    "revision": "ca0043d48bb429018446",
+    "url": "/simpu/static/js/2.e9f1ad4f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/simpu/static/js/2.e9f1ad4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd210e35276f969f9037",
+    "url": "/simpu/static/js/main.bc58b8a0.chunk.js"
+  },
+  {
+    "revision": "5ff7839c4960e4afcf94",
+    "url": "/simpu/static/js/runtime-main.259d6f9a.js"
+  },
+  {
+    "revision": "10a2082fe6b54f9786bd2d9a66f61536",
+    "url": "/simpu/static/media/analytics.10a2082f.svg"
+  },
+  {
+    "revision": "eec0e01959baef32032c7e255a250b3f",
+    "url": "/simpu/static/media/attachment.eec0e019.svg"
+  },
+  {
+    "revision": "fa41c1f6cd6620407c46dc3f83a1352c",
+    "url": "/simpu/static/media/comment.fa41c1f6.svg"
+  },
+  {
+    "revision": "4e738fd4ea3aa3c8e177895edaa9e1c1",
+    "url": "/simpu/static/media/happy.4e738fd4.svg"
+  },
+  {
+    "revision": "8ca5009bc60157eeb92fad9ec6eb8079",
+    "url": "/simpu/static/media/logo.8ca5009b.svg"
+  },
+  {
+    "revision": "3c8e7dfcff3b8c8e88c98bb921c579fd",
+    "url": "/simpu/static/media/schedule.3c8e7dfc.svg"
+  }
+]);
